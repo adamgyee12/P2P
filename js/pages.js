327c0289d41c1321ab451bf750a7fd2fa2ee4865
Adam Yee <adamgyee@umich.edu>
2017-06-08 15:27:34 -0400

    full walkthrough for challenge complete

---
+++ js/pages.js
@@ -0,0 +1,8 @@
+// Handle page transitions
+
+document.getElementById("join").onclick = function () {
+
+  location.href = "https://adamgyee12.github.io/P2P/comingsoon.html";
+    //location.href = "http://localhost:8000/giotto.html";
+    //https://adamgyee12.github.io/Portfolio/giotto.html
+};
